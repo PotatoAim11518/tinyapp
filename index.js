7cd156b2d64013196a8957f91c581e0ed2707bcc
wilson <whuang3015@gmail.com>
2021-09-14 16:09:59 -0700

    implement dark mode on grave location

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 window.addEventListener("DOMContentLoaded", () => {
 
+  const body = document.querySelector("body");
   const prev = document.getElementById("prev");
   const next = document.getElementById("next");
   const location = document.getElementById("location");
   const sections = document.querySelectorAll("a");
+  const navigation = document.querySelector("nav");
 
   let index = 0;
 
@@ -14,6 +16,19 @@ window.addEventListener("DOMContentLoaded", () => {
       index--;
     }
     location.innerText = sections[index].innerText;
+    if (location.innerText === "grave") {
+      body.classList.add('dark');
+      next.classList.add('dark');
+      prev.classList.add('dark');
+      navigation.classList.add('dark');
+      location.classList.add('spanDark');
+    } else {
+      body.classList.remove('dark');
+      next.classList.remove('dark');
+      prev.classList.remove('dark');
+      navigation.classList.remove('dark');
+      location.classList.remove('spanDark');
+    }
   })
 
   next.addEventListener('click', () => {
@@ -23,8 +38,23 @@ window.addEventListener("DOMContentLoaded", () => {
       index++;
     }
     location.innerText = sections[index].innerText;
+    if (location.innerText === "grave") {
+      body.classList.add('dark');
+      next.classList.add('dark');
+      prev.classList.add('dark');
+      navigation.classList.add('dark');
+      location.classList.add('spanDark');
+    } else {
+      body.classList.remove('dark');
+      next.classList.remove('dark');
+      prev.classList.remove('dark');
+      navigation.classList.remove('dark');
+      location.classList.remove('spanDark');
+    }
+
   })
 
-
+  location.addEventListener('change', () => {
+  })
 
 })
